ff00f62a413897972ce50c4bbfc65436b59bf957
Marvin1003 <marvin.klaproth@gmail.com>
2018-09-29 18:45:10 +0200

    updates vue config

--- configs/boilerplates/libraries/vue/minimal/webpack.config.js
+++ configs/boilerplates/libraries/vue/minimal/webpack.config.js
@@ -4,6 +4,8 @@ const webpackMerge = require("webpack-merge");
 const stats = require("./webpack-utils/stats");
 const loadPresets = require("./webpack-utils/presets/loadPresets");
 
+const { VueLoaderPlugin } = require("vue-loader");
+
 const modeConfig = ({ mode, port }) =>
   require(`./webpack-utils/configs/webpack.${mode}`)({ mode, port });
 
@@ -17,18 +19,20 @@ module.exports = ({
     {
       mode,
       stats,
-      resolve: {
-        vue$: "vue/dist/vue.esm.js"
-      },
       module: {
         rules: [
           {
             test: /\.js$/,
             include: path.resolve(__dirname, "src"),
             use: "babel-loader"
+          },
+          {
+            test: /\.vue$/,
+            use: "vue-loader"
           }
         ]
-      }
+      },
+      plugins: [new VueLoaderPlugin()]
     },
     modeConfig({ mode, port }),
     loadPresets({ mode, presets, options })
